4dbc2301105c21800ecc0f82485be67ecc3fa2ab
sya-ri <git@s7a.dev>
2024-05-24 15:21:30 +0900

    chore: Setup commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  // https://commitlint.js.org/#/reference-rules
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "test",
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "refactor",
+        "style",
+        "ci",
+        "perf",
+      ],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "apps",
+        "apps/backend",
+        "apps/catalog",
+        "apps/frontend",
+        "packages",
+        "packages/config",
+        "packages/ui",
+      ],
+    ],
+    "subject-case": [
+      2,
+      "always",
+      [
+        "lower-case", // default
+        "upper-case", // UPPERCASE
+        "camel-case", // camelCase
+        "kebab-case", // kebab-case
+        "pascal-case", // PascalCase
+        "sentence-case", // Sentence case
+        "snake-case", // snake_case
+        "start-case", // Start Case
+      ],
+    ],
+  },
+};
