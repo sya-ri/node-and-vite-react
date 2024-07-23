86930a6e251f7ba286da0394914e31412f069e76
sya-ri <git@s7a.dev>
2024-07-23 17:23:40 +0900

    chore: Bump dependencies

--- packages/openapi/backend-schema.ts
+++ packages/openapi/backend-schema.ts
@@ -3,127 +3,148 @@
  * Do not make direct changes to the file.
  */
 
-
 export interface paths {
-  "/todo": {
-    /**
-     * Get todo list
-     * @description Get todo list
-     */
-    get: operations["get_todo_list"];
-    /**
-     * Add todo
-     * @description Add todo
-     */
-    post: operations["add_todo"];
-  };
+    "/todo": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /**
+         * Get todo list
+         * @description Get todo list
+         */
+        get: operations["get_todo_list"];
+        put?: never;
+        /**
+         * Add todo
+         * @description Add todo
+         */
+        post: operations["add_todo"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
-
 export type webhooks = Record<string, never>;
-
 export interface components {
-  schemas: {
-    /** @description Response schema for getting todo list */
-    GetTodoResponse: {
-      /** @description Todo list */
-      list: components["schemas"]["TodoWithId"][];
+    schemas: {
+        /** @description Response schema for getting todo list */
+        GetTodoResponse: {
+            /** @description Todo list */
+            list: components["schemas"]["TodoWithId"][];
+        };
+        /** @description Request body schema for adding a new todo */
+        PostTodoRequestBody: {
+            todo: components["schemas"]["Todo"];
+        };
+        /** @description Response schema for adding a new todo */
+        PostTodoResponse: {
+            todo: components["schemas"]["WithId"];
+        };
+        /** @description Todo entity with an ID */
+        TodoWithId: components["schemas"]["WithId"] & components["schemas"]["Todo"];
+        /** @description Todo entity without an ID */
+        Todo: {
+            /**
+             * @description Display name
+             * @example Buy milk
+             */
+            name: string;
+        };
+        /** @description Schema that includes an ID */
+        WithId: {
+            /**
+             * Format: uuid
+             * @description Unique id
+             * @example a6363b15-02e0-4124-9a99-6a1a872acc33
+             */
+            id: string;
+        };
+        /** @description Schema for error responses */
+        ErrorResponse: {
+            /**
+             * @description Error message
+             * @example Internal Server Error
+             */
+            message: string;
+        };
     };
-    /** @description Request body schema for adding a new todo */
-    PostTodoRequestBody: {
-      todo: components["schemas"]["Todo"];
+    responses: {
+        /** @description Bad request */
+        BadRequestErrorResponse: {
+            headers: {
+                [name: string]: unknown;
+            };
+            content: {
+                "application/json": components["schemas"]["ErrorResponse"];
+            };
+        };
+        /** @description Internal Server Error */
+        InternalServerErrorResponse: {
+            headers: {
+                [name: string]: unknown;
+            };
+            content: {
+                "application/json": components["schemas"]["ErrorResponse"];
+            };
+        };
     };
-    /** @description Response schema for adding a new todo */
-    PostTodoResponse: {
-      todo: components["schemas"]["WithId"];
-    };
-    /** @description Todo entity with an ID */
-    TodoWithId: components["schemas"]["WithId"] & components["schemas"]["Todo"];
-    /** @description Todo entity without an ID */
-    Todo: {
-      /**
-       * @description Display name
-       * @example Buy milk
-       */
-      name: string;
-    };
-    /** @description Schema that includes an ID */
-    WithId: {
-      /**
-       * Format: uuid
-       * @description Unique id
-       * @example a6363b15-02e0-4124-9a99-6a1a872acc33
-       */
-      id: string;
-    };
-    /** @description Schema for error responses */
-    ErrorResponse: {
-      /**
-       * @description Error message
-       * @example Internal Server Error
-       */
-      message: string;
-    };
-  };
-  responses: {
-    /** @description Bad request */
-    BadRequestErrorResponse: {
-      content: {
-        "application/json": components["schemas"]["ErrorResponse"];
-      };
-    };
-    /** @description Internal Server Error */
-    InternalServerErrorResponse: {
-      content: {
-        "application/json": components["schemas"]["ErrorResponse"];
-      };
-    };
-  };
-  parameters: never;
-  requestBodies: never;
-  headers: never;
-  pathItems: never;
+    parameters: never;
+    requestBodies: never;
+    headers: never;
+    pathItems: never;
 }
-
 export type $defs = Record<string, never>;
-
-export type external = Record<string, never>;
-
 export interface operations {
-
-  /**
-   * Get todo list
-   * @description Get todo list
-   */
-  get_todo_list: {
-    responses: {
-      /** @description Success */
-      200: {
-        content: {
-          "application/json": components["schemas"]["GetTodoResponse"];
+    get_todo_list: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-      };
-      500: components["responses"]["InternalServerErrorResponse"];
-    };
-  };
-  /**
-   * Add todo
-   * @description Add todo
-   */
-  add_todo: {
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["PostTodoRequestBody"];
-      };
-    };
-    responses: {
-      /** @description Todo created successfully */
-      200: {
-        content: {
-          "application/json": components["schemas"]["PostTodoResponse"];
+        requestBody?: never;
+        responses: {
+            /** @description Success */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["GetTodoResponse"];
+                };
+            };
+            500: components["responses"]["InternalServerErrorResponse"];
         };
-      };
-      400: components["responses"]["BadRequestErrorResponse"];
-      500: components["responses"]["InternalServerErrorResponse"];
     };
-  };
+    add_todo: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["PostTodoRequestBody"];
+            };
+        };
+        responses: {
+            /** @description Todo created successfully */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["PostTodoResponse"];
+                };
+            };
+            400: components["responses"]["BadRequestErrorResponse"];
+            500: components["responses"]["InternalServerErrorResponse"];
+        };
+    };
 }
